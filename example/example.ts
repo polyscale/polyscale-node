f623f1fecbcdb99367a194af5d165ff31f752ba5
Jannik Keye <jannik.keye@gmail.com>
2022-02-10 11:28:42 +0100

    feat: make example testable

--- example/example.ts
+++ example/example.ts
@@ -1,8 +1,9 @@
-import { PolyScale } from "../src/main";
+import assert from "assert";
+import { CacheTtl, PolyScale } from "../src/main";
 
 const polyscale = new PolyScale({
   url: "http://localhost:8000",
-  apiKey: "some-api-key",
+  apiKey: process.env.API_KEY ?? "some-api-key",
 });
 
 /**
@@ -14,9 +15,42 @@ const init = async () => {
       name: "Example Cache",
       host: "localhost",
       port: 4000,
+      database: "mysql",
+      cachingEnabled: true,
+      defaultCachingBehaviour: "autoCached",
     });
 
     console.dir(cache);
+
+    let ttls: Array<CacheTtl>;
+
+    await polyscale.cacheTtl.create({
+      cacheId: cache.id,
+      type: "AUTO",
+      key: "some-template",
+    });
+
+    ttls = await polyscale.cacheTtl.getMany({ cacheId: cache.id });
+
+    assert(ttls.length === 1, "Expected 1 ttl");
+    assert(
+      ttls.find((d) => d.type === "AUTO"),
+      "Expected AUTO ttl"
+    );
+
+    await polyscale.cacheTtl.create({
+      cacheId: cache.id,
+      type: "MANUAL",
+      key: "some-template",
+    });
+
+    ttls = await polyscale.cacheTtl.getMany({ cacheId: cache.id });
+
+    assert(ttls.length === 1, "Expected 1 ttl");
+    assert(
+      ttls.find((d) => d.type === "MANUAL"),
+      "Expected MANUAL ttl"
+    );
 
     await polyscale.cacheTtl.create({
       cacheId: cache.id,
@@ -24,6 +58,10 @@ const init = async () => {
       value: 5,
       key: "tableA",
     });
+
+    ttls = await polyscale.cacheTtl.getMany({ cacheId: cache.id });
+
+    assert(ttls.length === 2, "Expected 2 ttls");
 
     await polyscale.cacheTtl.update({
       cacheId: cache.id,
@@ -36,7 +74,18 @@ const init = async () => {
     await polyscale.cacheTtl.delete({
       cacheId: cache.id,
       cacheTtlKey: "tableA",
+      cacheTtlType: "TABLE",
     });
+
+    await polyscale.cacheTtl.delete({
+      cacheId: cache.id,
+      cacheTtlKey: "some-template",
+      cacheTtlType: "MANUAL",
+    });
+
+    ttls = await polyscale.cacheTtl.getMany({ cacheId: cache.id });
+
+    assert(ttls.length === 0, "Expected 0 ttls");
 
     await polyscale.cache.delete({
       cacheId: cache.id,
@@ -46,4 +95,4 @@ const init = async () => {
   }
 };
 
-init();
+void init();
